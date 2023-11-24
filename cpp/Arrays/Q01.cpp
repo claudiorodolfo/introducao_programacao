d6d8dbc084e4727cb4429d9a65878fe547953d23
Cláudio Rodolfo Sousa de Oliveira <claudio.ifbavc@gmail.com>
2023-11-24 01:35:00 -0300

    Update Q01.cpp

--- cpp/Arrays/Q01.cpp
+++ cpp/Arrays/Q01.cpp
@@ -5,12 +5,21 @@ declare um array de inteiros e imprima seus
  */
 #include <iostream>
 #include <locale.h>
+//#include <vector>
+//#include <array>
 using namespace std;
+
 int main () {
 	setlocale(LC_ALL,"Portuguese");
+	
 	int dado[] = {2, 3, 9, 2, 1, 32, 0, -1};
 	const int tam = sizeof dado/sizeof dado[0];
 	
+	//array<int, 6> dado = {2, 3, 9, 2, 1, 32};
+	//vector<int> dado = {2, 3, 9, 2, 1, 32};	
+  	//const int tam = dado.size();
+  	//const int tam = size(dado);
+	    
 	for (int i = 0; i < tam; i++) {
 		cout<<dado[i]<<" ";
 	}
