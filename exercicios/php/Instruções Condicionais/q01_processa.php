fc6ff30e3ac50c70675e9c2bc218f74e77d266f9
Cláudio Rodolfo Sousa de Oliveira <claudio.ifbavc@gmail.com>
2024-03-07 01:47:02 -0300

    Instruções Condicionais em PHP

---
+++ exercicios/php/Instruções Condicionais/q01_processa.php
@@ -0,0 +1,27 @@
+<?php
+	$valor = $_POST['valor'];
+	
+	switch ($valor) {
+    case 1:
+        print 'Domingo';
+        break;
+    case 2:
+        print 'Segunda-feira';
+        break;
+    case 3:
+        print 'Terça-feira';
+        break;
+    case 4:
+        print 'Quarta-feira';
+        break;
+	case 5:
+        print 'Quinta-feira';
+        break;
+    case 6:
+        print 'Sexta-feira';
+        break;
+	case 7:
+        print 'Sábado';
+        break;
+	}
+?>
