f6f3dee750db2a3fdcf1c7ff623011b0ca4d18fc
Cláudio Rodolfo Sousa de Oliveira <claudio.ifbavc@gmail.com>
2024-03-07 01:50:00 -0300

    Update q01_processa.php

--- exercicios/php/Instruções Condicionais/q01_processa.php
+++ exercicios/php/Instruções Condicionais/q01_processa.php
@@ -2,26 +2,26 @@
 	$valor = $_POST['valor'];
 	
 	switch ($valor) {
-    case 1:
-        print 'Domingo';
-        break;
-    case 2:
-        print 'Segunda-feira';
-        break;
-    case 3:
-        print 'Terça-feira';
-        break;
-    case 4:
-        print 'Quarta-feira';
-        break;
+	case 1:
+		print 'Domingo';
+		break;
+	case 2:
+		print 'Segunda-feira';
+		break;
+	case 3:
+		print 'Terça-feira';
+		break;
+	case 4:
+		print 'Quarta-feira';
+		break;
 	case 5:
-        print 'Quinta-feira';
-        break;
-    case 6:
-        print 'Sexta-feira';
-        break;
+		print 'Quinta-feira';
+		break;
+	case 6:
+		print 'Sexta-feira';
+		break;
 	case 7:
-        print 'Sábado';
-        break;
+		print 'Sábado';
+		break;
 	}
 ?>
