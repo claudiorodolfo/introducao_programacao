42b564dbad30582ce0346ad13f064c052e7bb883
Cláudio Rodolfo Sousa de Oliveira <claudio.ifbavc@gmail.com>
2024-04-26 10:49:58 -0300

    Update Q23.cpp

--- exercicios/cpp/Instruções Condicionais/Q23.cpp
+++ exercicios/cpp/Instruções Condicionais/Q23.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <clocale>
 #include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
@@ -8,6 +9,7 @@ int main() {
 	setlocale(LC_ALL, "Portuguese");
 	string pessoa, computador;
 	
+	srand(time(NULL));
 	int sorteio = rand() % 3;
 	switch (sorteio) {
 		case 0:
@@ -26,28 +28,30 @@ int main() {
 	cin>>pessoa;
 	
 	if (pessoa == computador) {
-		cout<<"Empate";
+		cout<<"Empate"<<endl;
 	} else {
 		if (pessoa == "pedra") {
 			if (computador == "papel")
-				cout<<"Computador ganhou!";
+				cout<<"Computador ganhou!"<<endl;
 			else	//Tesoura
-				cout<<"Você ganhou!";				
+				cout<<"Você ganhou!"<<endl;	
 		}
 		
 		if (pessoa == "papel") {
 			if (computador == "tesoura")
-				cout<<"Computador ganhou!";
+				cout<<"Computador ganhou!"<<endl;
 			else	//pedra
-				cout<<"Você ganhou!";				
+				cout<<"Você ganhou!"<<endl;
 		}
 	
 		if (pessoa == "tesoura") {
 			if (computador == "pedra")
-				cout<<"Computador ganhou!";
+				cout<<"Computador ganhou!"<<endl;
 			else	//papel
-				cout<<"Você ganhou!";					
+				cout<<"Você ganhou!"<<endl;					
 		}
 	}
+	cout<<"Pessoa:    "<<pessoa<<endl;
+	cout<<"Computador:"<<computador<<endl;
 	return 0;	
 }
