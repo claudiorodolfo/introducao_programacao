e926cb25e3f26ea77913db9f6f00d7a501373065
Cláudio Rodolfo Sousa de Oliveira <claudio.ifbavc@gmail.com>
2025-09-12 13:04:31 -0300

    Add scripts to map numbers to weekdays
    Added Q01a.py using if-elif statements and Q01b.py using match-case to display the corresponding weekday for a number between 1 and 7. Both scripts handle invalid input with an error message.

---
+++ exercicios/python/Instruções Condicionais/Q01b.py
@@ -0,0 +1,20 @@
+# Programa que pede um número de 1 a 7 e mostra o dia da semana correspondente (usando match-case)
+numero = int(input("Digite um número de 1 a 7: "))
+
+match numero:
+    case 1:
+        print("Domingo")
+    case 2:
+        print("Segunda-feira")
+    case 3:
+        print("Terça-feira")
+    case 4:
+        print("Quarta-feira")
+    case 5:
+        print("Quinta-feira")
+    case 6:
+        print("Sexta-feira")
+    case 7:
+        print("Sábado")
+    case _:
+        print("Número inválido! Digite um valor entre 1 e 7.")
