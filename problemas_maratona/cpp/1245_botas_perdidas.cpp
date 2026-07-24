5b4297e33aacd64fcd7fcf695b075183df6644a1
Cláudio Rodolfo S. de Oliveira <31605720+claudiorodolfo@users.noreply.github.com>
2026-07-24 14:35:21 -0300

    Fix formatting and text in 1245_botas_perdidas.cpp

--- problemas_maratona/cpp/1245_botas_perdidas.cpp
+++ problemas_maratona/cpp/1245_botas_perdidas.cpp
@@ -6,21 +6,21 @@ de botas de vários tamanhos para seus soldados. No entanto, por uma falha de em
 da fábrica contratada, nem todas as caixas entregues continham um par de botas correto, 
 com duas botas do mesmo tamanho, uma para cada pé. O sargento mandou que os recrutas 
 retirassem todas as botas de todas as caixas para reembalá-las, desta vez corretamente.
-Quando o sargento descobriu que você sabia programar, ele solicitou com a gentileza habitual 
-que você escrevesse um programa que, dada a lista contendo a descrição de cada bota entregue, 
-determina quantos pares corretos de botas poderão ser formados no total.
+Quando o sargento descobriu que você sabia programar, ele solicitou com a gentileza 
+habitual que você escrevesse um programa que, dada a lista contendo a descrição de cada 
+bota entregue, determina quantos pares corretos de botas poderão ser formados no total.
 
 Entrada
 A entrada é composta por diversos casos de teste e termina com final de arquivo (EOF). A 
 primeira linha de um caso de teste contém um inteiro N (2 = N = 10 4), N é par, indicando 
-o número de botas individuais entregues. Cada uma das N linhas seguintes descreve uma bota, 
-contendo um número inteiro M (30 = M = 60) e uma letra L, separados por uma espaço em branco. 
-M indica o número da bota e L indica o pé da bota: L = 'D' indica que a bota é para o pé 
-direito, L = 'E' indica que a bota é para o pé esquerdo.
+o número de botas individuais entregues. Cada uma das N linhas seguintes descreve uma 
+bota, contendo um número inteiro M (30 = M = 60) e uma letra L, separados por uma espaço 
+em branco.  M indica o número da bota e L indica o pé da bota: L = 'D' indica que a bota 
+é para o pé direito, L = 'E' indica que a bota é para o pé esquerdo.
 
 Saída
-Para cada caso de teste imprima uma linha contendo um único número inteiro indicando o número 
-total de pares corretos que podem ser formados.
+Para cada caso de teste imprima uma linha contendo um único número inteiro indicando o 
+número total de pares corretos que podem ser formados.
 */
 #include <iostream>
 
