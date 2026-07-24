1bd3b43c43e701153c7e83eb17de9b73f1039e04
Cláudio Rodolfo S. de Oliveira <31605720+claudiorodolfo@users.noreply.github.com>
2026-07-24 14:38:31 -0300

    Improve clarity of digital laugh description
    Refactor text for clarity and consistency in the description of digital laughs.

--- problemas_maratona/cpp/2242_huaauhahhuahau.cpp
+++ problemas_maratona/cpp/2242_huaauhahhuahau.cpp
@@ -11,16 +11,17 @@ huehuehue
 
 Cláudia é uma jovem programadora que ficou intrigada pela sonoridade das “risadas digitais”. 
 Algumas delas ela nem mesmo consegue pronunciar! Mas ela percebeu que algumas delas parecem 
-transmitir melhor o sentimento da risada que outras. A primeira coisa que ela percebeu é que 
-as consoantes não interferem no quanto as risadas digitais influenciam na transmissão do 
-sentimento. A segunda coisa que ela percebeu é que as risadas digitais mais engra¸cadas são 
-aquelas em que as sequências de vogais são iguais quando lidas na ordem natural (da esquerda 
-para a direita) ou na ordem inversa (da direita para a esquerda), ignorando as consoantes. 
+transmitir melhor o sentimento da risada que outras. A primeira coisa que ela percebeu é 
+que as consoantes não interferem no quanto as risadas digitais influenciam na transmissão 
+do sentimento. A segunda coisa que ela percebeu é que as risadas digitais mais engra¸cadas 
+são aquelas em que as sequências de vogais são iguais quando lidas na ordem natural (da 
+esquerda para a direita) ou na ordem inversa (da direita para a esquerda), ignorando as 
+consoantes. 
 Por exemplo, “hahaha” e “huaauhahhuahau” estão entre as risadas mais engraçadas, enquanto 
 “riajkjdhhihhjak” e “huehuehue” não estão entre as mais engraçadas.
-Cláudia está muito atarefada com a análise estatística das risadas digitais e pediu sua ajuda 
-para escrever um programa que determine, para uma risada digital, se ela é das mais engraçadas 
-ou não.
+Cláudia está muito atarefada com a análise estatística das risadas digitais e pediu sua 
+ajuda para escrever um programa que determine, para uma risada digital, se ela é das mais 
+engraçadas ou não.
 
 Entrada
 A entrada é composta por uma linha, contendo uma sequência de no máximo 50 caracteres, formada 
