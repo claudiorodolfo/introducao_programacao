a35b30c72a2e0a9d65e7d8ce5a2ab77d289baa2e
Cláudio Rodolfo S. de Oliveira <31605720+claudiorodolfo@users.noreply.github.com>
2026-07-24 14:30:50 -0300

    Update 1103_alarme_despertador.cpp

--- problemas_maratona/cpp/1103_alarme_despertador.cpp
+++ problemas_maratona/cpp/1103_alarme_despertador.cpp
@@ -14,18 +14,20 @@ para dormir, acaba tendo dificuldades para adormecer e aproveitar o descanso.
 Um problema que a tem atormentado na hora de dormir é saber quantos minutos ela teria de 
 sono se adormecesse imediatamente e acordasse somente quando o despertador tocasse. Mas 
 ela realmente não é muito boa com números, e pediu sua ajuda para escrever um programa que, 
-dada a hora corrente e a hora do alarme, determine o número de minutos que ela poderia dormir.
+dada a hora corrente e a hora do alarme, determine o número de minutos que ela poderia 
+dormir.
 
 Entrada
-A entrada contém vários casos de teste. Cada caso de teste é descrito em uma linha, contendo 
-quatro números inteiros H1 , M1 , H2 e M2, com H1:M1 representando a hora e minuto atuais, e 
-H2:M2 representando a hora e minuto para os quais o alarme  despertador foi programado 
-(0≤H1≤23, 0≤M1≤59, 0≤H2≤23, 0≤M2 ≤59).
+A entrada contém vários casos de teste. Cada caso de teste é descrito em uma linha, 
+contendo quatro números inteiros H1 , M1 , H2 e M2, com H1:M1 representando a hora e 
+minuto atuais, e H2:M2 representando a hora e minuto para os quais o alarme  despertador 
+foi programado (0≤H1≤23, 0≤M1≤59, 0≤H2≤23, 0≤M2 ≤59).
 O final da entrada é indicado por uma linha que contém apenas quatro zeros, separados por 
 espaços em branco.
 
 Saída
-Para cada caso de teste da entrada seu programa deve imprimir uma linha, cada uma contendo um número inteiro, indicando o número de minutos que Daniela tem para dormir.
+Para cada caso de teste da entrada seu programa deve imprimir uma linha, cada uma contendo 
+um número inteiro, indicando o número de minutos que Daniela tem para dormir.
 */
 
 #include <iostream>
